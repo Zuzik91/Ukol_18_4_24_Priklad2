c44038c2e9272a3ab2e0a2aebb573f6acc950598
Zuzik91 <zuzana.smidova91@seznam.cz>
2024-04-22 21:11:39 +0200

    Cely priklad

---
+++ Ukol_18_4_24_Priklad2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ukol_18_4_24_Priklad2
+{
+    //Napište program, který se zeptá na počet hvězdiček a potom je v cyklu zobrazí na konzoli.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte počet hvězdiček:");
+
+            // Čtení vstupu od uživatele a převod na int
+            int pocetHvezdicek = int.Parse(Console.ReadLine());
+            // Cyklus pro zobrazení hvězdiček
+            for (int i = 0; i < pocetHvezdicek; i++)
+            {
+                Console.Write("*");
+            }
+            // Udržení konzole otevřené po dokončení cyklu
+            Console.WriteLine("\nStiskněte libovolnou klávesu pro ukončení...");
+            Console.ReadKey();
+        }
+    }
+}
